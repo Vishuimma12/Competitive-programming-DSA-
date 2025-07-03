dbad218491ea34d01c52a9a8743cc9a0da9a44b3
Vishal Singh <101267652+Vishuimma12@users.noreply.github.com>
2025-07-03 15:50:45 +0530

    Data type file is added

---
+++ Getting Started/dataType.cpp
@@ -0,0 +1,32 @@
+//
+// Created by vssin on 03-07-2025.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=300;
+    cout<<a<<endl;
+
+    char b = 'V';
+    cout<<b<<endl;
+
+    bool c=1;
+    cout<<c<<endl;
+
+    float d=3.14;
+    cout<<d<<endl;
+
+    double e=3.14;
+    cout<<e<<endl;
+
+    int size=sizeof(int);
+    int size2=sizeof(char);
+    int size3=sizeof(float);
+    int size4=sizeof(double);
+    cout<<size<<endl;
+    cout<<size2<<endl;
+    cout<<size3<<endl;
+    cout<<size4<<endl;
+
+}
---
+++ Getting Started/lessthangraterthan.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+    if(a>b) {
+        cout<<"A is greater than B";
+    }
+    else if(a<b){
+        cout<<"A is less than B";
+    }
+    else {
+        {
+            cout<<"A equal to B";
+        }
+    }
+}
