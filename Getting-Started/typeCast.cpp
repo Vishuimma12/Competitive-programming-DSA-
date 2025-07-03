2eadde3072c5224640bb8dfb2fa15f54a6d686dd
Vishal Singh <101267652+Vishuimma12@users.noreply.github.com>
2025-07-03 15:59:44 +0530

    Typecast int to car and char to int..

---
+++ Getting-Started/typeCast.cpp
@@ -0,0 +1,14 @@
+//
+// Created by vssin on 03-07-2025.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    int a='a';
+    cout<<a<<endl;
+
+    char ch = 98;  //it use ASCI code to print the value
+    cout<<ch<<endl;
+
+}
