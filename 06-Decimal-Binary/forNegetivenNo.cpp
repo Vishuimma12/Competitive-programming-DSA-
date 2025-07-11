5027c8979f4584e71d26311bcf03dfc16e905577
Vishal Singh <101267652+Vishuimma12@users.noreply.github.com>
2025-07-11 21:10:05 +0530

    using 2'c comliment

--- 06-Decimal-Binary/forNegetivenNo.cpp
+++ 06-Decimal-Binary/forNegetivenNo.cpp
@@ -2,31 +2,27 @@
 // Created by vssin on 10-07-2025.
 //
 #include <iostream>
-#include <math.h>
-
+#include <bitset>
 using namespace std;
 
 int main() {
-    unsigned int n;
+    int n;
     cout << "Enter a number: ";
     cin >> n;
-    int ans = 0;
-    int i=0;
-    while (n != 0) {
-        int bit = n & 1;
-        ans = (bit * pow(10,i)) +ans;
-        n = n >> 1;
-        i++;
-    }
-    cout << "Binary number is: " << ans << endl;
-    i =0;
-    int result = 0;
-    while ( ans != 0) {
-        ans= ans & 1;
-        result = ans + pow(2,i);
-        ans = ans >> 1;
-        i++;
-    }
-    cout << "Binary number is: " << result << endl;
 
+    // Use 8-bit representation for example
+    const int BITS = 8;
+
+    if (n >= 0) {
+        // Positive number: simple binary
+        bitset<BITS> binary(n);
+        cout << "Binary (unsigned) : " << binary << endl;
+    } else {
+        // Negative number: 2's complement
+        int twoComp = (1 << BITS) + n; // 2^BITS + n
+        bitset<BITS> binary(twoComp);
+        cout << "Binary (2's complement of " << n << "): " << binary << endl;
+    }
+
+    return 0;
 }
