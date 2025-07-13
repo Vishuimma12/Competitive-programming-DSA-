cd10dc2b24c46e87fd502efabffcea0e2e13a0a4
Vishal Singh <101267652+Vishuimma12@users.noreply.github.com>
2025-07-13 10:44:56 +0530

    Intro of Switch case

---
+++ 07-Switch-Case/switchCase.cpp
@@ -0,0 +1,55 @@
+//
+// Created by vssin on 13-07-2025.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    char ch ;
+    cin >> ch;
+    cout << endl;
+
+    switch (ch) {
+        case 'a':
+            cout << "Char "<<ch;
+            break;
+        case 'b':
+            cout << "Char "<<ch;
+            break;
+        case 'c':
+            cout << "Char "<<ch;
+            break;
+        case 'd':
+            cout << "Char "<<ch;
+            break;
+        case 'e':
+            cout << "Char "<<ch;
+            break;
+        case 'f':
+            cout << "Char "<<ch;
+            break;
+        case 'g':
+            cout << "Char "<<ch;
+            break;
+        case 'h':
+            cout << "Char "<<ch;
+            break;
+        case 'i':
+            cout << "Char "<<ch;
+            break;
+        case 'j':
+            cout << "Char "<<ch;
+            break;
+        case 'k':
+            cout << "Char "<<ch;
+            break;
+        case 'l':
+            cout << "Char "<<ch;
+            break;
+        case 'm':
+            cout << "Char "<<ch;
+            break;
+        default:
+            cout << "its an Defalut Character "<<ch;
+    }
+}
