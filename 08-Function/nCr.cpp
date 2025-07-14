ce978dd50fba39507f3bf1da1e49ca137baf28b4
Vishal Singh <101267652+Vishuimma12@users.noreply.github.com>
2025-07-14 23:38:07 +0530

    nCr implemented using multiple function.

---
+++ 08-Function/nCr.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+//
+// Created by vssin on 14-07-2025.
+//
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    int factorial = 1;
+    for (int i=1; i<=n; i++) {
+        factorial *= i;
+    }
+    return factorial;
+}
+int nCr(int n, int r) {
+    int num= factorial(n);
+    int deno =  factorial(r)*factorial(n-r);
+    return num/deno;
+}
+
+int main() {
+    int n;
+    cout << "Enter an integer for 'n' : ";
+    cin >> n;
+    int r;
+    cout << "Enter an integer for 'r' : ";
+    cin >> r;
+
+    int ans = nCr(n, r);
+    cout << "Ans of nCr is: " << ans << endl;
+    return 0;
+}
