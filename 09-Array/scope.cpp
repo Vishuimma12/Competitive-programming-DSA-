aeae9f2d0e851c73bdcf30f3d8c86a6db8d25c0d
Vishal Singh <101267652+Vishuimma12@users.noreply.github.com>
2025-07-16 23:39:54 +0530

    we cant update array out of the scope if we do it also change in main array.

--- 09-Array/scope.cpp
+++ 09-Array/scope.cpp
@@ -1,3 +1,30 @@
 //
 // Created by vssin on 16-07-2025.
 //
+
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+void update(int arr[], int n) {
+    arr[0]=120;
+    cout<< "Inside update: "<<endl;
+    printArray(arr, n);
+
+    cout << "Going Outside update... "<<endl;
+}
+
+int main() {
+    int arr[10]={1,2,3,4,5,6,7,8,9};
+    update(arr,10);
+
+    cout << endl;
+    cout<<"Printing the main function: "<<endl;  // it also assign with 120 at index 0... because it share the address...
+                                                 // it is not making an copy like an variable...
+    printArray(arr,10);
+}
