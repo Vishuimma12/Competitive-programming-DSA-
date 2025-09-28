d435d595acb837e33adcb198a6bada1281e37c30
Vishal Singh <101267652+Vishuimma12@users.noreply.github.com>
2025-09-28 12:06:48 +0530

    Solution for largest element in an array...

--- 09-Array/Problems/01-Question.cpp
+++ 09-Array/Problems/01-Question.cpp
@@ -5,39 +5,37 @@
 // Question: Find the largest element in an array.
 
 #include <iostream>
-#include <vector>
 using namespace std;
+// Brute force solution:-
+// void bubbleSort(int arr[], int n) {
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < n - i - 1; j++) {
+//             if (arr[j] > arr[j + 1]) {
+//                 swap(arr[j], arr[j + 1]);
+//             }
+//         }
+//     }
+// }
+//
+// int largestElement(int arr[], int n) {
+//     bubbleSort(arr, n);
+//     return arr[n - 1];
+// }
 
-int partition(vector<int>& arr, int low, int high) {
+//Optimal Solution:-
 
-    int pivot = arr[high];
-    int i = low - 1;
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(arr[i], arr[j]);
+void largestElement(int arr[], int n) {
+    int largest = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
         }
     }
-
-    swap(arr[i + 1], arr[high]);
-    return i + 1;
+    cout << largest << endl;
 }
 
-// the QuickSort function implementation
-void quickSort(vector<int>& arr, int low, int high) {
+int main(int argc, char *argv[]) {
+    int arr[6] =  { 10,1,7,6,14,9};
+    largestElement(arr, 6);
 
-    if (low < high) {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
-}
-
-int main() {
-    vector<int> arr = {10, 7, 8, 9, 1, 5};
-    int n = arr.size();
-    quickSort(arr, 0, n - 1);
-
-    cout<< "Last element is "<< arr[n-1] << endl;
-    return 0;
 }
