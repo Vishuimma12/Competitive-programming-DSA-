cac06e97712295feeee8d1cc9980e35f091c893f
Vishal Singh <101267652+Vishuimma12@users.noreply.github.com>
2025-10-03 20:25:17 +0530

    Updated Rotate by d place

--- 09-Array/Problems/06-Question.cpp
+++ 09-Array/Problems/06-Question.cpp
@@ -1,6 +1,8 @@
 //
 // Created by vssin on 01-10-2025.
 //
+
+#include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -12,25 +14,44 @@ void printArray(vector<int> arr) {
     cout << endl;
 }
 
+// TC -> O ( d ) + O ( n-d ) + o ( d ) = O (n+d)
+// SC -> o ( d )
+// void rotate_by_D_place(vector<int> &nums, int d) {
+//     int n = nums.size();
+//     d = d % n; // handle case where d > n
+//
+//     // Step 1: store first d elements
+//     vector<int> temp;
+//     for (int i = 0; i < d; i++) {
+//         temp.push_back(nums[i]);
+//     }
+//
+//     // Step 2: shift remaining n-d elements to the front
+//     for (int i = d; i < n; i++) {
+//         nums[i - d] = nums[i];
+//     }
+//
+//     // Step 3: put back stored elements at the end
+//     for (int i = 0; i < d; i++) {
+//         nums[n - d + i] = temp[i];
+//     }
+// }
+
+void reverse(vector<int> arr) {
+
+}
 void rotate_by_D_place(vector<int> &nums, int d) {
     int n = nums.size();
-    d = d % n; // handle case where d > n
+    d = d % n; // in case d > n
 
-    // Step 1: store first d elements
-    vector<int> temp;
-    for (int i = 0; i < d; i++) {
-        temp.push_back(nums[i]);
-    }
+    // Reverse first d elements
+    reverse(nums.begin(), nums.begin() + d);
 
-    // Step 2: shift remaining n-d elements to the front
-    for (int i = d; i < n; i++) {
-        nums[i - d] = nums[i];
-    }
+    // Reverse the remaining n-d elements
+    reverse(nums.begin() + d, nums.end());
 
-    // Step 3: put back stored elements at the end
-    for (int i = 0; i < d; i++) {
-        nums[n - d + i] = temp[i];
-    }
+    // Reverse the whole array
+    reverse(nums.begin(), nums.end());
 }
 
 int main() {
