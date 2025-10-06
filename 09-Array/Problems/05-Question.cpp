73fc52da33ac9732cfab413450a66f277a4848e9
Vishal Singh <101267652+Vishuimma12@users.noreply.github.com>
2025-10-06 19:20:25 +0530

    Updated

--- 09-Array/Problems/05-Question.cpp
+++ 09-Array/Problems/05-Question.cpp
@@ -1,3 +1,37 @@
 //
 // Created by vssin on 29-09-2025.
 //
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void rotateArray(vector<int>& nums, int k) {
+    int n = nums.size();
+    k %= n;
+
+    while(k--){
+        int temp = nums[n-1];
+
+        for (int j = n - 1; j > 0; j--) {
+            nums[j] = nums[j - 1]; // shift right
+        }
+        nums[0] = temp;
+    }
+
+
+    cout << "[";
+    for(int i = 0; i < n; i++){
+        cout << nums[i] << ",";
+    }
+    cout<< "]";
+
+}
+
+int main(int argc, char *argv[]) {
+    vector<int> nums = {1,2,3,4,5,6,7};
+    int k;
+    cout << "Enter the number to be rotated: ";
+    cin >> k;
+    rotateArray(nums, k);
+}
