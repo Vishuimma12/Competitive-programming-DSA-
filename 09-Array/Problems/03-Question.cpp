e5189f1b915b0b7fb7037fd26d97f9c1495d659b
Vishal Singh <101267652+Vishuimma12@users.noreply.github.com>
2025-10-06 19:19:50 +0530

    Updated

--- 09-Array/Problems/03-Question.cpp
+++ 09-Array/Problems/03-Question.cpp
@@ -1,3 +1,25 @@
 //
 // Created by vssin on 28-09-2025.
 //
+//Problem: Check if the array is sorted for not
+#include <iostream>
+using namespace std;
+
+bool checkArraySorted(int arr[], int n) {
+
+    for (int i = 0; i < n-1; i++) {
+        if (arr[i] <= arr[i+1]) {
+
+        }
+        else {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(int argc, char *argv[]) {
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7,8, 9, 10};
+    cout << checkArraySorted(arr,10) << endl;
+}
+
