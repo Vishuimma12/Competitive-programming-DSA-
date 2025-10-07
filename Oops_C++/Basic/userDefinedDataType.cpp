8ab96baad9a9d419afe5f7c79b5c811b6340e3dc
Vishal Singh <101267652+Vishuimma12@users.noreply.github.com>
2025-10-07 21:45:55 +0530

    Add user defined data type example

--- Oops_C++/Basic/userDefinedDataType.cpp
+++ Oops_C++/Basic/userDefinedDataType.cpp
@@ -1,3 +1,32 @@
 //
 // Created by vssin on 07-10-2025.
 //
+
+#include <iostream>
+using namespace std;
+
+class Student { // Student is a new data type.
+    public:
+    string name;
+    int roll_no;
+    float cgpa;
+};
+
+int main(int argc, char *argv[]) {
+    Student s1;
+    s1.name = "Vishal";
+    s1.roll_no = 10;
+    s1.cgpa = 3.5;
+    cout << s1.roll_no << endl;
+    cout << s1.cgpa << endl;
+    cout << s1.name << endl;
+
+    Student s2;
+    s2.name = "jatin";
+    s2.roll_no = 11;
+    s2.cgpa = 3.0;
+    cout << s2.cgpa << endl;
+    cout << s2.name << endl;
+    cout << s2.roll_no << endl;
+
+}
