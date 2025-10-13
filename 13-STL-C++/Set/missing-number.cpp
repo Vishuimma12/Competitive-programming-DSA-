0a91068d5ea269f6d6804dd955461edcbdf95c29
Vishal Singh <101267652+Vishuimma12@users.noreply.github.com>
2025-10-13 10:13:07 +0530

    Added STL Set missing-number.cpp

--- 13-STL-C++/Set/missing-number.cpp
+++ 13-STL-C++/Set/missing-number.cpp
@@ -1,3 +1,20 @@
 //
 // Created by vssin on 12-10-2025.
 //
+
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int arr[]= {0,4,2,1,5};
+    set <int> s;
+
+    for(int i=0;i<5;i++) { // insertion
+        s.emplace(arr[i]);
+    }
+    for (int x : s) {  //print set
+        cout << x << " ";
+    };
+
+}
