dbf0c962c0db48cd85b8adcf2ede2d0ee74b8de2
Vishal Singh <101267652+Vishuimma12@users.noreply.github.com>
2025-10-13 10:13:08 +0530

    Added 2D-Array intro.cpp

--- 14-2D-Array/intro.cpp
+++ 14-2D-Array/intro.cpp
@@ -1,3 +1,23 @@
 //
 // Created by vssin on 09-10-2025.
 //
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    int arr[3][4];
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+}
